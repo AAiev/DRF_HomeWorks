052c6d8ef42513a4e6cfce3069d3db3704099ef2
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-03-05 12:34:21 +0300

    наполнение модели User

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE: dict[str, bool] = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=150, verbose_name='страна', **NULLABLE)
+    avatar = models.ImageField(upload_to='user_avatar/', verbose_name='аватар', **NULLABLE)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
