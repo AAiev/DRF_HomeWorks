52719bc9b008432a5cca60c8823258cc5f1454de
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-03-05 18:04:56 +0300

    описание сериализаторов, урлов и вьюшек для моделей Course и Lesson для приложения study

---
+++ study/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    image_preview = models.ImageField(verbose_name='превью-картинка', upload_to='image/', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='Описание')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_image/', verbose_name='Изображение', **NULLABLE)
+    url_video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
