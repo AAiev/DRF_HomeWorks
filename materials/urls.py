5e101da5c15961e9551554dd71301b726ab0c49f
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-04-06 23:29:18 +0300

    * apps.py и в urls.py исправил опечатку
    написал тесты для приложения Materials

--- materials/urls.py
+++ materials/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path
 
-from materials.apps import StudyConfig
+from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
 
 from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
     LessonUpdateAPIView, LessonDestroyAPIView, SubscribeToUpdateAPIView
 
-app_name = StudyConfig.name
+app_name = MaterialsConfig.name
 
 router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
-                  path('lesson/create/', LessonCreateAPIView.as_view(), name='courses-create'),
-                  path('lesson/', LessonListAPIView.as_view(), name='courses-list'),
-                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='courses-get'),
-                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='courses-update'),
-                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='courses-delete'),
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 
                   path('course/subscribe/', SubscribeToUpdateAPIView.as_view(), name='subscribe'),
 
